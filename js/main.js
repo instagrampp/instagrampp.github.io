f8d4d265bb4d4639c83b08d70b1b04ad563f9fa8
instagrampp <instagramprofilepicture@yandex.com>
2018-04-05 02:43:58 +0300

    Timeout error fixed!

--- js/main.js
+++ js/main.js
@@ -4,15 +4,14 @@ $('#find').click(function(e){
 
     var username = $("input[name=username]").val();
     var url = "https://instagram.com/"+username+"?__a=1";
-    var success = false;
     var result;
     document.getElementById("loading").style.display = "-webkit-inline-box";
     document.getElementById("danger").style.display = "none";
     document.getElementById("profile").style.display = "none";
+    
     $.getJSON(url, function(data) {
         url = "https://i.instagram.com/api/v1/users/"+data.graphql.user.id+"/info/";
         $.getJSON(url, function(data) {
-            success = true;
             result = data.user.hd_profile_pic_url_info.url;
             document.getElementById("img-profile-picture").src = result;
             document.getElementById("profile").style.display = "-webkit-inline-box";
@@ -20,15 +19,11 @@ $('#find').click(function(e){
             document.getElementById("loading").style.display = "none";
             document.getElementById("danger").style.display = "none";
         });
-    });
-    
-    setTimeout(function() {
-        if (!success)
-        {
+    })
+    .fail(function(){
             document.getElementById("loading").style.display = "none";
             document.getElementById("danger").style.display = "-webkit-inline-box";
-        }
-    }, 6000);
+    });
 });
 
 $('#modal').on('hidden.bs.modal', function (e) {
