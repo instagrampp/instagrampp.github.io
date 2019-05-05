121ce2b1b30789cc80f33df58aeb7e6e3c8dc99f
instagrampp <37849833+instagrampp@users.noreply.github.com>
2019-05-05 22:35:43 +0300

    stories

--- js/main.js
+++ js/main.js
@@ -20,6 +20,15 @@ function field_control_download(ev2) {
      }
 }
 
+function field_control_stories(ev3) {
+     if(ev3.value){
+         $('#letseestories').prop( "disabled", false );
+     }
+     else{
+         $('#letseestories').prop( "disabled", true );
+     }
+}
+
 $('#letseeit').click(function(e){
 
     e.preventDefault();
@@ -93,21 +102,21 @@ $('#letseeit').click(function(e){
             url = "https://i.instagram.com/api/v1/users/"+user_id+"/info/";
             $.getJSON(url, function(data) {
                 result = data.user.hd_profile_pic_url_info.url;
-                $( ".carousel-indicators").append("<li data-target='#carousel' data-slide-to='0' class='active'></li>")
-                $( ".carousel-inner" ).append("<div class='carousel-item active'><a href='"+result+"' target='_blank'><img class='img-fluid' id='   img-profile-picture' src='"+result+"'></a></div>");
+                $( "#carousel-indicators-picture").append("<li data-target='#carousel' data-slide-to='0' class='active'></li>")
+                $( "#carousel-inner-picture").append("<div class='carousel-item active'><a href='"+result+"' target='_blank'><img class='img-fluid' id='   img-profile-picture' src='"+result+"'></a></div>");
                 if(is_private == false){
                     $( "#letseeit-control-prev").show();
                     $( "#letseeit-control-next").show();
                     for(i=0; i<array.length; i++){
-                            $( ".carousel-indicators").append("<li data-target='#carousel' data-slide-to='"+(i+1)+"'></li>");
-                            $( ".carousel-inner" ).append("<div class='carousel-item'><a href='"+array[i]+"' target='_blank'><img class='img-fluid' id='img-profile-picture' src='"+array[i]+"'></a></div>");
+                            $( "#carousel-indicators-picture").append("<li data-target='#carousel' data-slide-to='"+(i+1)+"'></li>");
+                            $( "#carousel-inner-picture" ).append("<div class='carousel-item'><a href='"+array[i]+"' target='_blank'><img class='img-fluid' id='img-profile-picture' src='"+array[i]+"'></a></div>");
                         }
                 }
                 $("#profile").attr("href", "https://instagram.com/"+username);
                 $( "#profile").show();
                 $( "#see-more").show();
                 $( "#loading").hide();
-                $( "#danger").hide();;
+                $( "#danger").hide();
             });
         }
         if(xmlHttp.status == 404){
@@ -125,8 +134,8 @@ function error(){
 }
 
 $('#modal').on('hidden.bs.modal', function (e) {
-        $( ".carousel-indicators").empty();
-        $( ".carousel-inner" ).empty();
+        $( "#carousel-indicators-picture").empty();
+        $( "#carousel-inner-picture" ).empty();
         $( "#loading").hide();
         $('#photo_url').css("border", "1px solid black");
 })
@@ -135,9 +144,10 @@ $('#download_modal').on('hidden.bs.modal', function (e) {
         $( "#modal-body-download").empty();
 })
 
-$('#stories_modal').on('hidden.bs.modal', function (e) {
-        $( ".stories-inner").empty();
-        $('.modal-title-stories').empty();
+$('#modal2').on('hidden.bs.modal', function (e) {
+        $( "#carousel-indicator-stories").empty();
+        $( "#carousel-inner-stories" ).empty();
+        $( "#loading2").hide();
 })
 
 $('#download').click(function(e){
@@ -209,4 +219,64 @@ $(' #see-more').click(function(e){
     $(' #see-more-header').toggle(200);
     
 });
+
+
+$('#letseestories').click(function(e){
+
+    e.preventDefault();
+    var username_story = $("input[name=story_url]").val();
+    $('.invalid-feedback').hide();
+    $( "#loading2").fadeIn();
+    $( "#danger").hide();
+    $( "#profile").hide();
+    $( "#modal-body").css("background", "#141d26");
+    $( "#letseestories-control-prev").hide();
+    $( "#letseestories-control-next").hide();
+
+    $.ajax({
+    type:'POST',
+    origin:'Access-Control-Allow-Origin: *',
+    url:'https://apiv2.insta-stories.ru/stories',
+    data:{input:username_story},
+        success:function(data){
+            console.log(data);
+            $( "#loading2").fadeOut();
+            $( "#letseestories-control-prev").show();
+            $( "#letseestories-control-next").show();
+
+            if(data.stories[0].video){
+                $( "#carousel-indicators-stories").append("<li data-target='#carousel2' data-slide-to='0' class='active'></li>")
+                $( "#carousel-inner-stories" ).append("<div class='carousel-item active'><a href='"+data.stories[0].video.url+"' target='_blank'><img class='img-fluid' id='img-profile-picture' src='"+data.stories[0].video.url+"'></a></div>");
+             
+            }
+            else{
+                $( "#carousel-indicators-stories").append("<li data-target='#carousel2' data-slide-to='0' class='active'></li>")
+                $( "#carousel-inner-stories" ).append("<div class='carousel-item active'><a href='"+data.stories[0].img+"' target='_blank'><img class='img-fluid' id='img-profile-picture' src='"+data.stories[0].img+"'></a></div>");
+            }
+            if (data.stories.length > 0){
+                for(var i=0; i<data.stories.length-1; i++){
+                    if(data.stories[i+1].video){
+                        $( "#carousel-indicators-stories").append("<li data-target='#carousel2' data-slide-to='"+(i+1)+"'></li>");
+                        $("#carousel-inner-stories").append("<div class='carousel-item'><video class='d-flex justify-content-center img-fluid' controls id='player'><source src="+data.stories[i+1].video.url+" type='video/mp4'></video></div>");
+                        //$( "#carousel-indicators-stories").hide();
+                    }
+                    else{
+                        $( "#carousel-indicators-stories").append("<li data-target='#carousel2' data-slide-to='"+(i+1)+"'></li>");
+                        $( "#carousel-inner-stories" ).append("<div class='carousel-item'><a href='"+data.stories[i+1].img+"' target='_blank'><img class='img-fluid' id='img-stories' src='"+data.stories[i+1].img+"'></a></div>");
+                    }
+                }
+            }
+        },
+        fail: function(){
+            console.log("hi");
+        }
+
+});
+
+
+});
+
+
+
+
 
